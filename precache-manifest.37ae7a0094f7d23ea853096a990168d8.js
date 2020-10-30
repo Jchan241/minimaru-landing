1b54393287674c2c079442691f8b2255a95ec95d
jc <johchan24@gmail.com>
2020-10-30 12:40:34 +1100

    Updates

---
+++ precache-manifest.37ae7a0094f7d23ea853096a990168d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01848fab946a9fe42693442ccf1fa63b",
+    "url": "/minimaru-landing/index.html"
+  },
+  {
+    "revision": "7cd04ae9e3dbdb04e195",
+    "url": "/minimaru-landing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e7006c29ad561a8c285a",
+    "url": "/minimaru-landing/static/js/2.8ec8fc10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minimaru-landing/static/js/2.8ec8fc10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd04ae9e3dbdb04e195",
+    "url": "/minimaru-landing/static/js/main.155bce55.chunk.js"
+  },
+  {
+    "revision": "7da54e7d2eb77b1a579c",
+    "url": "/minimaru-landing/static/js/runtime-main.29e30957.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.127714a8c66d555558bb53db16d63eb4.js"
+  "/minimaru-landing/precache-manifest.37ae7a0094f7d23ea853096a990168d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minimaru-landing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
